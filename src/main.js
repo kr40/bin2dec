e114c6f03075098fedcab07943702f1fb326b748
kr40 <kr40.git@gmail.com>
2021-05-25 21:45:47 +0530

    changed imports

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
-// This is a Module, use index.js for your code instead.
+'use strict';
+//* Imports
+
+import { binaryVal, convertBtn, decimalVal, resetBtn, switchThemeBtn } from './modules/selectors';
+
+//* Convert function
+
+convertBtn.addEventListener('click', function () {
+	if (binaryVal.value === '') alert('Please Enter a Binary Number!');
+	binaryVal.value.split('').map((char) => {
+		if (char !== '0' && char !== '1') alert(`${binaryVal.value} is not a valid Binary Number!`);
+	});
+	const decimal = parseInt(binaryVal.value, 2);
+	decimalVal.value = decimal;
+});
+
+//* Reset Function
+
+resetBtn.addEventListener('click', function () {
+	binaryVal.value = '';
+	decimalVal.value = '';
+});
+
+//* Switch Theme Function
+
+switchThemeBtn.addEventListener('click', function () {
+	const theme = document.getElementsByTagName('link')[6];
+
+	if (theme.getAttribute('href') == 'https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css') {
+		theme.setAttribute('href', 'https://cdn.jsdelivr.net/npm/water.css@2/out/light.css');
+	} else {
+		theme.setAttribute('href', 'https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css');
+	}
+});
---
+++ src/modules/selectors.js
@@ -0,0 +1,7 @@
+// * All selectors
+
+export const binaryVal = document.getElementById('binaryVal');
+export const decimalVal = document.getElementById('decimalVal');
+export const convertBtn = document.querySelector('.convertBtn');
+export const resetBtn = document.querySelector('.resetBtn');
+export const switchThemeBtn = document.querySelector('.switchThemeBtn');
